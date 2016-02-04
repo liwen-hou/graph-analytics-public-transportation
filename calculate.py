cdaaa3f2bd1b2855edc640bdb580135a17576bbb
liwen-hou <henryhouliwen@gmail.com>
2016-02-04 14:27:48 +0000

    add calculate.py

--- calculate.py
+++ calculate.py
@@ -9,11 +9,9 @@ import operator
 import graph_tool.all as gt
 
 def calculate_centrality(graph):
-	cl_unweighted = gt.closeness(graph)
-	cl_distance = gt.closeness(graph,weight=graph.ep.Distance)
-	bt_unweighted, ep = gt.betweenness(graph)
-	bt_distance, ep = gt.betweenness(graph,weight=graph.ep.Distance)
+	print 'calculate closeness'
 	cl_time = gt.closeness(graph,weight=graph.ep.Time)
+	print 'calculate betweenness'
 	bt_time, ep = gt.betweenness(graph,weight=graph.ep.Time)
 
 	f = open('cl_time.txt', 'w+')
@@ -22,29 +20,43 @@ def calculate_centrality(graph):
 	f = open('bt_time.txt', 'w+')
 	f = open('bt_time.txt', 'r+')
 	f.writelines(["%s\n" % item  for item in bt_time.a])
-	f = open('cl_unweighted.txt', 'w+')
-	f = open('cl_unweighted.txt', 'r+')
-	f.writelines(["%s\n" % item  for item in cl_unweighted.a])
-	f = open('cl_distance.txt', 'w+')
-	f = open('cl_distance.txt', 'r+')
-	f.writelines(["%s\n" % item  for item in cl_distance.a])
-	f = open('bt_unweighted.txt', 'w+')
-	f = open('bt_unweighted.txt', 'r+')
-	f.writelines(["%s\n" % item  for item in bt_unweighted.a])
-	f = open('bt_distance.txt', 'w+')
-	f = open('bt_distance.txt', 'r+')
-	f.writelines(["%s\n" % item  for item in bt_distance.a])
 
 	with open('results_time.csv','wb') as results:
 		writer = csv.writer(results,delimiter=',')
-		header = ['Name','Type','Longitude','Latitude','Closeness_Unweighted','Closeness_Distance','Betweenness_Unweighted','Betweenness_Distance','Closeness_Time','Betweenness_Time']
+		header = ['Name','Type','Longitude','Latitude','Closeness_Time','Betweenness_Time']
 		writer.writerow(header)
 		for v in graph.vertices():
-			row = [graph.vp.name[v],graph.vp.Type[v],graph.vp.Longitude[v],graph.vp.Latitude[v],cl_unweighted[v],cl_distance[v],bt_unweighted[v],bt_distance[v],cl_time[v],bt_time[v]]
+			row = [graph.vp.name[v],graph.vp.Type[v],graph.vp.Longitude[v],graph.vp.Latitude[v],cl_time[v],bt_time[v]]
 			writer.writerow(row)
 
 if __name__ == '__main__':
 
 	graph = load_graph('w_time_sg_graph.graphml')
-
+	print 'graph loaded'
+	e = graph.edge(27752,50854)
+	graph.ep.Time[e] = 19
+	e = graph.edge(34494,4816)
+        graph.ep.Time[e] = 30
+	e = graph.edge(36011,31250)
+        graph.ep.Time[e] = 13
+	e = graph.edge(55605,107841)
+        graph.ep.Time[e] = 7
+	e = graph.edge(85227,4816)
+        graph.ep.Time[e] = 13
+	e = graph.edge(97669,77485)
+        graph.ep.Time[e] = 7
+	e = graph.edge(125822,45118)
+        graph.ep.Time[e] = 7
+	for e in graph.edges():
+		if graph.ep.Time[e] < 0:
+			print e, ' ', graph.ep.Time[e], ' ', graph.ep.Method[e],' ', graph.ep.Distance[e]
+			graph.ep.Time[e] = 2
+	print 'time adjusted'
+	graph.save('w_time_sg_graph.graphml')
+	print 'graph saved'
+	graph = load_graph('w_time_sg_graph.graphml')
+	print 'graph reloaded'
+	for e in graph.edges():
+                if graph.ep.Time[e] < 0:
+                        print e, ' ', graph.ep.Time[e], ' ', graph.ep.Method[e],' ', graph.ep.Distance[e]
 	calculate_centrality(graph)
