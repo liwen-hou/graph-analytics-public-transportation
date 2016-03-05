1b415704125098b2c332330589f206233fb71f57
liwen-hou <henryhouliwen@gmail.com>
2016-03-06 03:52:39 +0800

    add consolidate.py

--- consolidate.py
+++ consolidate.py
@@ -18,7 +18,7 @@ def init_db():
 def combine(db):
 
 	cur = db.cursor()
-	command = 'SELECT count(*) '#h.postcode, p.lon, p.lat, h.block, h.floor, h.age, h.size, h.price, h.rooms, p.clt, p.cld, p.clg,mt.mrt1, mt.dist1, mt.mrt2, mt.dist2, mt.mrt3, mt.dist3, sh.school1, sh.dist1, sh.school2, sh.dist2, sh.school3, sh.dist3, ml.mall1, ml.dist1, ml.mall2, ml.dist2, ml.mall3, ml.dist3 '
+	command = 'SELECT h.postcode, p.lon, p.lat, h.block, h.floor, h.age, h.size, h.price, h.rooms, p.clt, p.cld, p.clg,mt.mrt1, mt.dist1, mt.mrt2, mt.dist2, mt.mrt3, mt.dist3, sh.school1, sh.dist1, sh.school2, sh.dist2, sh.school3, sh.dist3, ml.mall1, ml.dist1, ml.mall2, ml.dist2, ml.mall3, ml.dist3 '
 	command = command + 'FROM hdb h, postcode p, nearschool sh, nearmrt mt, nearmall ml '
 	command = command + 'WHERE h.postcode = p.name AND h.postcode = sh.postcode AND h.postcode = ml.postcode AND h.postcode = mt.postcode'
 	cur.execute(command)
@@ -32,5 +32,5 @@ if __name__ == '__main__':
 
     db = init_db()
     #os.chdir('./HDBPriceFor2013')
-    add_school(db)
+    combine(db)
     db.close()
